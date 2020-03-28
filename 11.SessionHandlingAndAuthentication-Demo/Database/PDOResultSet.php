e8130de25abb2f0e99297fb8978bac0ad82b56d5
Kalin Motovilkov <monyllakk@abv.bg>
2020-03-28 17:23:03 +0200

    Create PDOResultSet.php

---
+++ 11.SessionHandlingAndAuthentication-Demo/Database/PDOResultSet.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database;
+
+use Generator;
+
+class PDOResultSet implements ResultSetInterface
+{
+
+
+    private $pdoStatement;
+
+    public function __construct(\PDOStatement $PDOStatement)
+    {
+        $this->pdoStatement = $PDOStatement;
+    }
+
+    /**
+     * @param $className
+     * @return Generator
+     */
+    public function fetch($className): Generator
+    {
+        while ($row = $this->pdoStatement->fetchObject($className)) {
+            yield $row;
+        }
+    }
+}
