7d4df7ca698820abb08ed00d585cea8e021b7197
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-03 19:13:32 +0300

    Update 05.MaxSequenceOfEqualElements.php

--- 02.AdvancedSyntaxAndOperations-Exercise/05.MaxSequenceOfEqualElements.php
+++ 02.AdvancedSyntaxAndOperations-Exercise/05.MaxSequenceOfEqualElements.php
@@ -1,27 +1,19 @@
 <?php
 $numbers = explode(" ", readline());
-$count = 0;
-$bestCount = 0;
-$num = "";
+$maxCount = 0;
+$maxCountNum = "";
 for ($i = 0; $i < count($numbers); $i++) {
     $count = 0;
     for ($j = $i; $j < count($numbers); $j++) {
-        $current = $numbers[$i];
-        $next = $numbers[$j];
-        if ($current == $next) {
+        if ($numbers[$i] == $numbers[$j]) {
             $count++;
-            if ($count > $bestCount) {
-                $bestCount = $count;
-                $num = $current;
+            if ($count > $maxCount) {
+                $maxCount = $count;
+                $maxCountNum = $numbers[$i];
             }
-        } else {
-            $count = 0;
-        }
-        if ($bestCount == 1) {
-            $num = $numbers[0];
+        }else{
+            $count=0;
         }
     }
 }
-for ($k = 0; $k < $bestCount; $k++) {
-    echo "$num ";
-}
+echo str_repeat("$maxCountNum ",$maxCount);
