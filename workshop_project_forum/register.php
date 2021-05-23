970144d5e1e6a87f422a26061069f3392bc5bd5a
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-23 21:20:50 +0300

    Create register.php

---
+++ workshop_project_forum/register.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'db/db_connection.php';
+$response = "";
+
+if (isset($_POST["username"], $_POST["password"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    require_once 'db/user_queries.php';
+
+    $result = register($db, $username, $password);
+    if ($result) {
+        header("Location: login.php");
+        exit;
+    } else {
+        $responce = "Error!";
+    }
+}
+require_once 'templates/register_form.php';
