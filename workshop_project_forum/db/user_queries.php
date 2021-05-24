92c85936ce3f69229b5e2748711fd3945ec09413
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-24 18:26:16 +0300

    Update user_queries.php

--- workshop_project_forum/db/user_queries.php
+++ workshop_project_forum/db/user_queries.php
@@ -1,8 +1,40 @@
 <?php
+function issueAuthenticatinoString(PDO $db, int $userId): string
+{
+    $query = "SELECT auth_string FROM authentications WHERE user_id=?";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$userId]);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($data && $data['auth_string']) {
+        return $data['auth_string'];
+    }
+    $authString = uniqid();
+    $query = "INSERT INTO  authentications (auth_string, user_id) values (?,?)";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$authString, $userId]);
+    return $authString;
+}
+
+function verifyCredentials(PDO $db, string $username, string $password): int
+{
+    $query = "SELECT id, password FROM users WHERE username = ?";
+    $stmt = $db->prepare($query);
+    if (!$stmt->execute([$username])) {
+        return -1;
+    }
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $passwordHash = $user['password'];
+    $result = password_verify($password, $passwordHash);
+    if ($result) {
+        return (int)$user['id'];
+    }
+    return -1;
+}
+
 function register(PDO $db, string $username, string $password): bool
 {
     $query = "INSERT INTO users(username, password ) VALUES (?, ?)";
-    $statement = $db->prepare($query);
-    $result = $statement->execute([$username, password_hash($password, PASSWORD_ARGON2I)]);
+    $stmt = $db->prepare($query);
+    $result = $stmt->execute([$username, password_hash($password, PASSWORD_ARGON2I)]);
     return $result;
 }
