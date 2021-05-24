2bd5d3da0ea2a4b74f6a2eedf56eb44dc5fbde5c
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-24 18:28:29 +0300

    Update register_form.php

--- workshop_project_forum/templates/register_form.php
+++ workshop_project_forum/templates/register_form.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register form</title>
+    <link rel="stylesheet" href="templates/styles/all.css">
 </head>
 <body>
-<form method="post">
-    Username <input type="text" name="username"/><br/>
-    Password <input type="password" name="password"/><br/>
-    <input type="submit"/>
-</form>
-<div id="response"><?=$response;?></div>
+<div class="container">
+<!--    <h1>Register form</h1>-->
+    <form method="post">
+        <div class="inputs">
+        Username <input type="text" value="<?= $username; ?>" name="username"/><br/>
+        Password <input type="<?= !empty($password) ? 'text' : 'password';?>" value="<?=$password;?>" name="password"/><br/>
+        </div>
+        <input id="submit" type="submit" value="Register"/>
+    </form>
+</div>
+<div id="response"><?= $response; ?></div>
 </body>
 </html>
