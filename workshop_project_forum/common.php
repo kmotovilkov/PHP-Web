9a7c6ca0cbd22c6bf3dbf1f89a5356b594f95949
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-25 23:33:33 +0300

    Update common.php

--- workshop_project_forum/common.php
+++ workshop_project_forum/common.php
@@ -2,12 +2,12 @@
 require_once 'db/db_connection.php';
 require_once 'db/user_queries.php';
 
-if (!isset($_GET['auth_id'])) {
+if (!isset($_GET['authId'])) {
     header("Location: login.php");
     exit;
 }
 
-$authId = $_GET['auth_id'];
+$authId = $_GET['authId'];
 $userId = getUserByAuthId($db, $authId);
 if ($userId == -1) {
     header("Location: login.php");
