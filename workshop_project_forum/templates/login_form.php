6cb9d60cadd01570d2ef4ae8ef168d419187b34a
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-25 18:27:02 +0300

    Update login_form.php

--- workshop_project_forum/templates/login_form.php
+++ workshop_project_forum/templates/login_form.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register form</title>
     <link rel="stylesheet" href="templates/styles/all.css">
-    <title>Login form</title>
 </head>
 <body>
 <div class="container">
-    <a class="go" href="././register.php">go to regster form</a>
-<form method="post">
-    <div class="inputs">
-    Username <input type="text" value="<?= $username; ?>" name="username"/><br/>
-    Password <input type="<?= !empty($password) ? 'text' : 'password';?>" value="<?=$password;?>" name="password"/><br/>
-    </div>
-    <input id="submit" type="submit" value="Login"/>
-</form>
+    <a class="go" href="././login.php">go to login form</a>
+    <form method="post">
+        <div class="inputs">
+            Username <input type="text" value="<?= $username; ?>" name="username"/><br/>
+            Password <input type="<?= !empty($password) ? 'text' : 'password'; ?>" value="<?= $password; ?>"
+                            name="password"/><br/>
+        </div>
+        <input id="submit" type="submit" value="Register"/>
+    </form>
 </div>
 <div id="response"><?= $response; ?></div>
 </body>
