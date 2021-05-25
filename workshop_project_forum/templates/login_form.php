278619edba70de3f8a89c95cc4f8f554a700082c
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-25 23:24:25 +0300

    Update login_form.php

--- workshop_project_forum/templates/login_form.php
+++ workshop_project_forum/templates/login_form.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register form</title>
     <link rel="stylesheet" href="templates/styles/all.css">
+    <title>Login form</title>
 </head>
 <body>
 <div class="container">
-    <a class="go" href="././login.php">go to login form</a>
+    <a class="go" href="././register.php">go to register form</a>
     <form method="post">
         <div class="inputs">
             Username <input type="text" value="<?= $username; ?>" name="username"/><br/>
             Password <input type="<?= !empty($password) ? 'text' : 'password'; ?>" value="<?= $password; ?>"
                             name="password"/><br/>
         </div>
-        <input id="submit" type="submit" value="Register"/>
+        <input id="submit" type="submit" value="Login"/>
     </form>
 </div>
 <div id="response"><?= $response; ?></div>
