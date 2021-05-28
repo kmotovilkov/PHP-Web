1558f8a533dcb14f8b3d4779948a9b9e4e3b04fd
Kalin Motovilkov <monyllakk@abv.bg>
2021-05-28 17:41:48 +0300

    Update 07.CreatingConstructor.php

--- 07.DefiningClasses-Lab/07.CreatingConstructor.php
+++ 07.DefiningClasses-Lab/07.CreatingConstructor.php
@@ -1,23 +1,19 @@
 <?php
 
-$name = readline();
-$age = readline();
-
 class Person
 {
     public $name;
     public $age;
 
-    /**
-     * Person constructor.
-     * @param $name
-     * @param $age
-     */
     public function __construct(string $name, int $age)
     {
         $this->name = $name;
         $this->age = $age;
         echo $this->name . " " . $this->age;
     }
+
 }
-$person = new Person($name, $age);
+$name=readline();
+$age=readline();
+
+$person = new Person($name,$age);
